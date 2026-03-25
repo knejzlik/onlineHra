9e21b5c9c996878a515f35f4b7d9142d909d0d50
brana <brana@PC1101>
2026-03-25 11:13:08 +0100

    qwe

--- onlineHra/Commands/Help.cs
+++ onlineHra/Commands/Help.cs
@@ -13,30 +13,30 @@ namespace onlineHra.Commands
         {
             var sb = new StringBuilder();
 
-            sb.AppendLine("=== NÁPOVĚDA ===");
-            sb.AppendLine("Seznam příkazů:");
+            sb.AppendLine("=== HELP ===");
+            sb.AppendLine("List of commands:");
             sb.AppendLine();
 
-            sb.AppendLine("jdi <směr/místnost>");
-            sb.AppendLine("  - pohyb mezi místnostmi");
+            sb.AppendLine("go <direction/room>");
+            sb.AppendLine("  - move between rooms");
 
-            sb.AppendLine("prozkoumej");
-            sb.AppendLine("  - zobrazí informace o aktuální místnosti");
+            sb.AppendLine("explore");
+            sb.AppendLine("  - shows information about the current room");
 
-            sb.AppendLine("inventar");
-            sb.AppendLine("  - zobrazí obsah inventáře");
+            sb.AppendLine("inventory");
+            sb.AppendLine("  - displays the contents of your inventory");
 
-            sb.AppendLine("vezmi <předmět>");
-            sb.AppendLine("  - vezme předmět z místnosti");
+            sb.AppendLine("take <item>");
+            sb.AppendLine("  - picks up an item from the room");
 
-            sb.AppendLine("odloz <předmět>");
-            sb.AppendLine("  - odloží předmět do místnosti");
+            sb.AppendLine("drop <item>");
+            sb.AppendLine("  - drops an item into the room");
 
-            sb.AppendLine("mluv <jméno>");
-            sb.AppendLine("  - interakce s NPC postavou");
+            sb.AppendLine("talk <name>");
+            sb.AppendLine("  - interact with an NPC character");
 
-            sb.AppendLine("pomoc");
-            sb.AppendLine("  - zobrazí tuto nápovědu");
+            sb.AppendLine("help");
+            sb.AppendLine("  - displays this help");
 
             return sb.ToString();
         }
